5540d3fdc7185411172fb2788559d19c6ec4abad
famturiop <vorobyev.seva.rules@gmail.com>
2014-07-12 00:54:50 +0400

    autopilot

---
+++ InterfaceClassLibrary3/InterfaceClassLibrary3/Class1.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace InterfaceClassLibrary3
+{
+    public class InterfaceClass
+    {
+        public static int vvod()
+        {
+            int command = Convert.ToInt32(Console.ReadLine());
+            return command;
+        }
+    }
+}
